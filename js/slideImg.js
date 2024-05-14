c1f2463545d45ef5dfb137c1cb9c2ae9ea0998ab
Camille <camille.morfin@etu.univ-lyon1.fr>
2024-05-14 23:51:21 +0200

    correction texte

--- js/slideImg.js
+++ js/slideImg.js
@@ -11,8 +11,8 @@ const App = Vue.createApp({
         <div class="divProjet">
             <p class="textSlide">Réalisé en Java, ce projet a pour but d'aider un responsable de plus de 30 établissements 
             à prendre des décisions, via une interface IHM. 
-            Ce logiciel a pour but d'aider à connaître les meilleurs chemins à emprunter en fonction de la fiabilité, 
-            de la durée et de la distance la plus courte.</p>
+            Ce logiciel a pour but de calculer les meilleurs chemins à emprunter en fonction de leur fiabilité, 
+             durée et distance.</p>
               <Slide
                   img1="./img/Graph/Principale.png"
                   img2="./img/Graph/chargement.png"
@@ -27,19 +27,18 @@ const App = Vue.createApp({
                   img3="./img/vroumvroum/creationCircuit.png"
               />
           <p class="textSlide">Ce projet est un site web réalisé en HTML, JavaScript et CSS. Il s'agit d'un jeu de course 
-          de voiture. Très fortement inspiré de Trackmania et de Geometry Dash, nous avons implémenté avec mon groupe la 
-          possibilité de créer nos propres circuits et de les rendre jouables par les autres utilisateurs. Nous pouvons 
-          également jouer à des circuits prêts et ceux des autres joueurs. Enfin, grâce à la monnaie mise en place dans 
-          le jeu (VroumCoins), nous pouvons personnaliser nos karts.</p>
+          de voiture très fortement inspiré de Trackmania et de Geometry Dash. Avec mon groupe, nous avons implémenté la 
+          création de circuits et leur utilisation par les autres joueurs. Enfin, grâce à la monnaie mise en place dans 
+          le jeu (VroumCoins), les joueurs peuvent acheter et personnaliser leurs karts.</p>
         </div>
         <h3>Gogo Itinero :</h3>
         <img id="logo" src="./img/gogoItinero/logo.png" alt="logo application gogoItinero">
         <div class="divProjet">
-          <p class="textSlide">Ce projet est une application Android, réalisée à l'aide de Android Studio. Grâce à cette 
-          application, nous pouvons prévisualiser des itinéraires que nous pouvons enregistrer en favoris. Nous pouvons 
-          également créer un compte à l'aide de Firebase pour pouvoir conserver nos itinéraires si nous changeons d'appareil. 
-          Enfin, quelques paramètres ont été mis en place comme le changement de mode (sombre/clair), mais aussi la sauvegarde 
-          ou non de l'historique, et enfin la désactivation de la musique de début.</p>
+          <p class="textSlide">Ce projet est une application Android, réalisée à l'aide de Android Studio. Elle permet 
+          de prévisualiser et d'enregistrer des itinéraires GPS automobiles à l'aide de l'API Google Maps. Nous pouvons 
+          également créer un compte à l'aide de Firebase pour conserver nos itinéraires en cas de changement d'appareil. 
+          Enfin, quelques paramètres ont été mis en place comme le changement de mode d'affichage (sombre/clair), la sauvegarde 
+          ou non de l'historique, et l'activation ou non de la musique de début.</p>
           <Slide
               img1="./img/gogoItinero/carte.png"
               img2="./img/gogoItinero/historique.png"
@@ -53,12 +52,14 @@ const App = Vue.createApp({
           
           <p class="textSlide">Réalisé en JavaScript, HTML et CSS, ce projet a été réalisé pour aider à l'organisation 
           et au bon déroulement d'un loto.
-          Deux modes de jeux sont proposés : le premier en mode automatique, c'est-à-dire que le tirage des nombres est 
-          fait automatiquement par la machine, et le deuxième mode est quant à lui manuel, c'est-à-dire qu'il doit y 
-          avoir une personne à côté de la machine qui, avec un boulier par exemple, fait le tirage des nombres et ensuite 
-          les sélectionne sur le site.
-          Au moment de jouer, l'organisateur peut également choisir la composition de sa partie en fonction de son règlement. 
-          Enfin, nous retrouvons sur la page de jeux un leaderboard à droite qui affiche le dernier nombre tiré et celui d'avant.</p>
+          Deux modes de jeux sont proposés :
+          <ul>
+            <li>en mode automatique, le tirage des nombres est fait automatiquement par la machine et</li>
+            <li>en mode manuel, c'est une personne physique à côté de la machine
+          qui effectue le tirage, par exemple avec un boulier, et entre les numéros tirés sur l'application.</li>
+          </ul>
+          Au moment de jouer, l'organisateur peut également choisir le déroulement de sa partie en fonction de son règlement. 
+          Enfin, sur la droite de la page du jeu, nous retrouvons un leaderboard qui affiche les deux derniers numéros tirés.</p>
           <Slide
               img1="./img/Loto/PrincipaleLoto.png"
               img2="./img/Loto/ModeJeuLoto.png"
